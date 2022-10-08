ba2585d3e453340de97ec7870089cb13951c614f
Willian Menezes <willian_menezes_santos@hotmail.com>
2022-10-08 15:45:04 -0300

    teste merge a2

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -2,3 +2,9 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
