2758b86d44724741c046b7aa1e1e410118782dd0
Willian Menezes <willian_menezes_santos@hotmail.com>
2022-10-08 15:40:28 -0300

    remotooooooooo

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
